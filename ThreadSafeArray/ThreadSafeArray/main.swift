9f814644842de9af1efbd52a879928d6a7b5c677
Orianka13 <odobrogor@gmail.com>
2021-11-05 12:09:59 +0300

    Реализованы базовые методы и объявлены базовые свойства

--- ThreadSafeArray/ThreadSafeArray/main.swift
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -7,5 +7,48 @@
 
 import Foundation
 
-print("Hello, World!")
+class ThreadSafeArray<Element> {
+    
+    private var threadSafeArray = [Element]()
+    
+    private var isEmpty: Bool {
+           return threadSafeArray.isEmpty
+       }
+    
+    private var count: Int {
+        return threadSafeArray.count
+    }
+    
+    
+    func append(_ item: Element) {
+        //async
+        threadSafeArray.append(item)
+        
+    }
+    
+    func remove(at index: Int) {
+        //async
+        threadSafeArray.remove(at: index)
+        
+    }
+    
+    subscript(index: Int) -> Element? {
+        //sync
+        var element: Element?
+        if threadSafeArray.indices.contains(index){
+            element = threadSafeArray[index]
+            return element
+        } else {
+            print("Нет элемента с указанным индексом")
+        }
+        return element
+    }
+}
 
+
+extension ThreadSafeArray where Element: Equatable {
+    func contains(_ element: Element) -> Bool {
+       //sync
+            return threadSafeArray.contains(element)
+    }
+}
