2fb225dc29f3306a40540175016b38fcdc67a74d
Orianka13 <odobrogor@gmail.com>
2021-11-05 12:30:35 +0300

    Создана очередь, выполнение методов поставлено в очередь

--- ThreadSafeArray/ThreadSafeArray/main.swift
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -12,43 +12,45 @@ class ThreadSafeArray<Element> {
     private var threadSafeArray = [Element]()
     
     private var isEmpty: Bool {
-           return threadSafeArray.isEmpty
-       }
+        return threadSafeArray.isEmpty
+    }
     
     private var count: Int {
         return threadSafeArray.count
     }
     
+    private let queue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
     
     func append(_ item: Element) {
-        //async
-        threadSafeArray.append(item)
-        
+        queue.async {
+            self.threadSafeArray.append(item)
+        }
     }
     
     func remove(at index: Int) {
-        //async
-        threadSafeArray.remove(at: index)
-        
+        queue.async {
+            self.threadSafeArray.remove(at: index)
+        }
     }
     
     subscript(index: Int) -> Element? {
-        //sync
-        var element: Element?
-        if threadSafeArray.indices.contains(index){
-            element = threadSafeArray[index]
+        queue.sync {
+            var element: Element?
+            if threadSafeArray.indices.contains(index){
+                element = threadSafeArray[index]
+                return element
+            } else {
+                print("Нет элемента с указанным индексом")
+            }
             return element
-        } else {
-            print("Нет элемента с указанным индексом")
         }
-        return element
     }
 }
 
-
 extension ThreadSafeArray where Element: Equatable {
     func contains(_ element: Element) -> Bool {
-       //sync
+        queue.sync {
             return threadSafeArray.contains(element)
+        }
     }
 }
