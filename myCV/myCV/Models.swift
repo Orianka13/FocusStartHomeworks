5642c64ffda5e5acfd3ac906bc06c45e5656675b
Orianka13 <odobrogor@gmail.com>
2021-11-12 16:10:28 +0300

    Создание проекта

---
+++ myCV/myCV/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  myCV
+//
+//  Created by Олеся Егорова on 12.11.2021.
+//
+
+import Foundation
---
+++ myCV/myCV/PickerViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  PickerViewManager.swift
+//  myCV
+//
+//  Created by Олеся Егорова on 12.11.2021.
+//
+
+import Foundation
