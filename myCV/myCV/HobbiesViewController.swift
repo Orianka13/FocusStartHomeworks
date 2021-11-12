2ef9fb0423a45f4a1e015d81c0af3b816195a9e6
Orianka13 <odobrogor@gmail.com>
2021-11-12 17:40:54 +0300

    Третий экран - Хобби (фото, текст)

--- myCV/myCV/HobbiesViewController.swift
+++ myCV/myCV/HobbiesViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class HobbiesViewController: UIViewController {
+    var hobbies = Hobbies()
+    
+    lazy var photoView: UIImageView = {
+        let image = UIImage(named: hobbies.imageGallery.first ?? "knit")
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var textView: UILabel = {
+        let text = hobbies.text.first
+        let textLabel = UILabel(frame: .zero)
+        textLabel.text = text
+        textLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 18)
+        textLabel.textColor = .black
+        textLabel.numberOfLines = 0
+        return textLabel
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubviews()
+        setConstraints()
+    }
+    private func addSubviews(){
+        view.addSubview(photoView)
+        view.addSubview(textView)
+    }
+    private func setConstraints(){
+        setPhotoView()
+        setTextView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setPhotoView(){
+        self.photoView.translatesAutoresizingMaskIntoConstraints = false
+        self.photoView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.photoView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -150).isActive = true
+        self.photoView.heightAnchor.constraint(equalToConstant: 300).isActive = true
     }
-    */
-
+    
+    private func setTextView(){
+        self.textView.translatesAutoresizingMaskIntoConstraints = false
+        self.textView.topAnchor.constraint(equalTo: self.photoView.bottomAnchor, constant: 20).isActive = true
+        self.textView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        self.textView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        self.textView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
 }
