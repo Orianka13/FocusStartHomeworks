e18ddf0533c745b5ca36572df93c03d1da55da5d
Orianka13 <odobrogor@gmail.com>
2021-11-17 13:45:44 +0300

    Создание проекта CollectionApp, добавление его в workspace

---
+++ CollectionApp/CollectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionApp
+//
+//  Created by Олеся Егорова on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
