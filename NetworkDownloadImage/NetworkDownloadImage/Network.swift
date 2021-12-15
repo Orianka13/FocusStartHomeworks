a0055692de65eb510423fa80abce1757fe87862d
Orianka13 <odobrogor@gmail.com>
2021-12-15 18:03:33 +0300

    Загрузка в фоне

--- NetworkDownloadImage/NetworkDownloadImage/Network.swift
+++ NetworkDownloadImage/NetworkDownloadImage/Network.swift
@@ -11,17 +11,46 @@ protocol INetworkService {
     func loadData(url: String, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
-final class NetworkService {
+final class NetworkService: NSObject {
     
-    private let session: URLSession
+    private var session: URLSession
+    private var bgSession: URLSession
     
-    init(configuration: URLSessionConfiguration? = nil) {
+    private lazy var bgConfiguration: URLSessionConfiguration = {
+        let configuration = URLSessionConfiguration.background(withIdentifier: "background")
+        configuration.sessionSendsLaunchEvents = true
+        configuration.isDiscretionary = true
+        configuration.allowsCellularAccess = true
+        configuration.shouldUseExtendedBackgroundIdleMode = true
+        configuration.waitsForConnectivity = true
+        return configuration
+    }()
+    
+    init(configuration: URLSessionConfiguration? = nil, bgConfiguration: URLSessionConfiguration? = nil) {
+        if let bgConfiguration = bgConfiguration {
+            self.bgSession = URLSession(configuration: bgConfiguration)
+        }
+        else {
+            self.bgSession = URLSession(configuration: URLSessionConfiguration.default)
+        }
+        
         if let configuration = configuration {
             self.session = URLSession(configuration: configuration)
         }
         else {
             self.session = URLSession(configuration: URLSessionConfiguration.default)
         }
+    }
+    
+    func start(url: String) {
+        guard let url = URL(string: url) else { return }
+        
+        let request = URLRequest(url: url)
+        
+        let task = bgSession.downloadTask(with: request)
+        task.countOfBytesClientExpectsToSend = 512
+        task.countOfBytesClientExpectsToReceive = 10 * 1024 * 1024
+        task.resume()
     }
 }
 
@@ -30,9 +59,9 @@ extension NetworkService: INetworkService {
     
     func loadData(url: String, completion: @escaping (Result<Data, Error>) -> Void) {
         guard let url = URL(string: url) else { return }
-
+        
         let request = URLRequest(url: url)
-        self.session.downloadTask(with: request) { data, response, error in
+        let task = self.session.downloadTask(with: request) { data, response, error in
             if let error = error {
                 completion(.failure(error))
             }
@@ -45,8 +74,47 @@ extension NetworkService: INetworkService {
                     completion(.failure(error))
                 }
             }
-        }.resume()
+        }
+        DispatchQueue.main.async {
+            
+            
+            let appDelegate = AppDelegate()
+            appDelegate.bgSessionCompletionHandler = {
+                task.cancel() { data in
+                    let task: URLSessionDownloadTask
+                    if let data = data {
+                        task = self.bgSession.downloadTask(withResumeData: data)
+                    } else {
+                        task = self.bgSession.downloadTask(with: request) { url, response, error in
+                            if let error = error {
+                                completion(.failure(error))
+                            }
+                            
+                            if let url = url {
+                                if let result = try? Data(contentsOf: url) {
+                                    completion(.success(result))
+                                }
+                            }
+                        }
+                    }
+                    task.resume()
+                }
+                
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
+
+
+extension NetworkService: URLSessionDelegate {
+    
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        
     }
 }
+
 
 
--- NetworkDownloadImage/NetworkDownloadImage/Support/AppDelegate.swift
+++ NetworkDownloadImage/NetworkDownloadImage/Support/AppDelegate.swift
@@ -9,6 +9,13 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var bgSessionCompletionHandler: (() -> ())?
+    
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        
+        bgSessionCompletionHandler = completionHandler
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
